dfc5298b4acaabfbc2f1ace5182c472e32b9a753
Paulo Henrique <p.h.moreirabarbosa@hotmail.com>
2016-03-20 22:40:57 -0300

    Commiti Desenho Abstrato Java

--- br/univel/classeabstrata/DesenhoAbstrato.java
+++ br/univel/classeabstrata/DesenhoAbstrato.java
@@ -5,9 +5,9 @@ import br.univel.interfaces.Desenho;
 public abstract class DesenhoAbstrato implements Desenho {
 
 	public void identificar() {
-		System.out.println("");
+		System.out.println("=============================");
 		System.out.println(getNome());
-		System.out.println("===================================");
+		System.out.println("=============================");
 		System.out.println("\n");
 	}
 	
